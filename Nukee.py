acda49d394726a84eed1578912e96149eed9279b
Samu369 <80537223+Samu369@users.noreply.github.com>
2024-02-22 20:56:41 +0100

    aggiunta dashboard

--- Nukee.py
+++ Nukee.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def start():
-    return render_template("start.html")
+def index():
+    return redirect("/nukee/login")
+
+@app.route("/nukee/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/nukee/dashboard", methods=["POST"])
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
