89da2fd73cfcd055affd158c3e1a9f597bab60de
Samu369 <80537223+Samu369@users.noreply.github.com>
2024-02-23 22:35:11 +0100

    modificata dashboard

--- Nukee.py
+++ Nukee.py
@@ -2,17 +2,25 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def index():
     return redirect("/nukee/login")
 
 @app.route("/nukee/login")
 def login():
-    return render_template("login.html")
+    global tk_v
+    return render_template("login.html", tk_v=tk_v)
 
 @app.route("/nukee/dashboard", methods=["POST"])
 def dashboard():
+    if request.form["token"] == "":
+        global tk_v
+        tk_v = "is-invalid"
+        return redirect("/nukee/login")
     return render_template("dashboard.html")
 
 if __name__ == "__main__":
+    tk_v = ""
     app.run(debug=True)
