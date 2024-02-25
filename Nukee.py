0480c997c80f8d039194e83914177bef62081852
Samu369 <80537223+Samu369@users.noreply.github.com>
2024-02-25 20:31:23 +0100

    modificata dashboard, reso elimina canali(1) funzionante, aggiunto caricamento(loading.html)

--- Nukee.py
+++ Nukee.py
@@ -1,26 +1,111 @@
 from flask import Flask, render_template, request, redirect
+import discord, asyncio, threading, werkzeug.serving
 
+
+# --- CONSTANTS ---
+DEBUGGER = True
+
+
+# --- Discord Checks ---
+async def check_token(token: str):
+    client = discord.Client(intents=discord.Intents.default())
+    try:
+        await client.login(token)
+        await client.close()
+        return True
+    except discord.errors.LoginFailure:
+        return False
+
+async def check_server(token: str, server_id:int):
+    client = discord.Client(intents=discord.Intents.default())
+    ret = False
+    
+    @client.event
+    async def on_ready():
+        nonlocal ret
+        server = client.get_guild(server_id)
+        if server is not None:
+            ret = True
+        await client.close()
+        
+    await client.start(token)
+    return ret
+
+
+# --- Discord Functions ---
+def ch_delete(token: str, serverid: int): # Elimina tutti i canali
+    try:
+        intents = discord.Intents.default()
+        intents.all()
+        client = discord.Client(intents=intents)
+        @client.event
+        async def on_ready():
+            print('--- ELIMINAZIONE CANALI ---')
+            server = client.get_guild(serverid)
+            if server:
+                canali = server.channels
+                for c in canali:
+                    try:
+                        await c.delete()
+                        print(f'Canale eliminato: {c.name} | {c.id}')
+                    except Exception as e:
+                        print(f'[{e}] Impossibile eliminare: {c.name} | {c.id}')
+                        continue
+                print("--- OPERAZIONE TERMINATA ---\n")
+            else:
+                print("IMPOSSIBILE TROVARE IL SERVER\n")
+            await client.close()        
+        client.run(token)
+    except Exception as e:
+        print(f"ERRORE: {e}\n")
+
+
+# --- Flask ---
 app = Flask(__name__)
-
-
+tk = ""
+sid = 0
 
 @app.route("/")
 def index():
-    return redirect("/nukee/login")
+    return redirect("/nukee")
 
-@app.route("/nukee/login")
+@app.route("/nukee", methods=["GET", "POST"])
 def login():
-    global tk_v
-    return render_template("login.html", tk_v=tk_v)
+    global tk, sid
+    if request.method == "GET":
+        return render_template("login.html", tk_v=tk, ids_v=sid)
+    elif request.method == "POST":
+        r = request.form.get("r", "0")
+        if r != "0":
+            return render_template("loading.html", r=r)
+        tk = request.form["token"]
+        try: 
+            sid = int(request.form["server-id"])
+        except ValueError: 
+            sid = 0
+        if asyncio.run(check_token(tk)) == True:
+            tk_v = "is-valid"
+            if asyncio.run(check_server(tk, sid)) == True:
+                sid_v = "is-valid"
+                return render_template("dashboard.html", hidden="hidden")
+            else:
+                sid_v = "is-invalid"
+        else:
+            tk_v = "is-invalid"
+            sid_v = ""
+        return render_template("login.html", tk_v=tk_v, ids_v=sid_v)
 
-@app.route("/nukee/dashboard", methods=["POST"])
-def dashboard():
-    if request.form["token"] == "":
-        global tk_v
-        tk_v = "is-invalid"
-        return redirect("/nukee/login")
-    return render_template("dashboard.html")
+@app.route("/nukee/r", methods=["POST"])
+def r():
+    if request.form["r"] == "1":
+        ch_delete(token=tk, serverid=sid)
+        return render_template("dashboard.html", hidden="")
+
 
 if __name__ == "__main__":
-    tk_v = ""
-    app.run(debug=True)
+    def start_flask():
+        global DEBUGGER
+        werkzeug.serving.run_simple("localhost", 5000, app, use_reloader=False, use_debugger=DEBUGGER)
+    flask_thread = threading.Thread(target=start_flask)
+    flask_thread.start()
+    flask_thread.join()
